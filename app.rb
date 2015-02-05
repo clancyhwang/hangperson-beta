43fcfc756aa07e7ec3bde95cf6f375259655b527
clancy <clancyhwang@berkeley.edu>
2015-02-05 00:48:49 -0800

    done

--- app.rb
+++ app.rb
@@ -35,11 +35,18 @@ class HangpersonApp < Sinatra::Base
   end
   
   # Use existing methods in HangpersonGame to process a guess.
-  # If a guess is repeated, set flash[:message] to "You have already used that letter."
+
   # If a guess is invalid, set flash[:message] to "Invalid guess."
   post '/guess' do
     letter = params[:guess].to_s[0]
     ### YOUR CODE HERE ###
+    if not @game.guess(letter)
+      if @game.repeated
+        flash[:message] = "You have already used that letter."
+      else
+        flash[:message] = "Invalid guess."
+      end
+    end
     redirect '/show'
   end
   
@@ -50,17 +57,34 @@ class HangpersonApp < Sinatra::Base
   # @wrong_guesses and @word_with_guesses, so set those up here.
   get '/show' do
     ### YOUR CODE HERE ###
+    @wrong_guesses = @game.wrong_guesses
+    @word_with_guesses = @game.word_with_guesses
+    if @game.check_win_or_lose == :win
+      redirect '/win'
+    elsif @game.check_win_or_lose == :lose
+      redirect '/lose'
+    end
     erb :show # You may change/remove this line
   end
   
   get '/win' do
     ### YOUR CODE HERE ###
-    erb :win # You may change/remove this line
+    if @game.check_win_or_lose != :win
+      redirect '/show'
+    else
+      @word = @game.word
+      erb :win # You may change/remove this line
+    end
   end
   
   get '/lose' do
     ### YOUR CODE HERE ###
-    erb :lose # You may change/remove this line
+    if @game.check_win_or_lose != :lose
+      redirect '/show'
+    else
+      @word = @game.word
+      erb :lose # You may change/remove this line
+    end
   end
   
 end
--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -3,8 +3,59 @@ class HangpersonGame
   # add the necessary class methods, attributes, etc. here
   # to make the tests in spec/hangperson_game_spec.rb pass.
 
-  # Get a word from remote "random word" service
+  attr_accessor :word, :guesses, :wrong_guesses, :repeated
 
+  def initialize(word)
+    @word = word
+    @guesses = ''
+    @wrong_guesses = ''
+  end
+
+  def guess(letter)
+    @repeated = false
+    if letter.nil? or letter == '' or letter =~ /^[^a-z]$/i
+      raise ArgumentError
+    end
+    letter = letter[0].downcase
+
+    if @guesses.include?(letter) or @wrong_guesses.include?(letter)
+      @repeated = true
+    end
+
+    if @word.include?(letter) and not @guesses.include?(letter)
+      @guesses += letter
+
+    elsif not @word.include?(letter) and not @wrong_guesses.include?(letter)
+      @wrong_guesses += letter
+
+    else
+      false
+    end
+  end
+
+  def word_with_guesses
+    display_word = ""
+    @word.gsub(/./) do |letter|
+      if @guesses.include?(letter)
+        display_word += letter
+      else
+        display_word += '-'
+      end
+    end
+    display_word
+  end
+
+  def check_win_or_lose
+    if @word == word_with_guesses
+      :win
+    elsif @wrong_guesses.length >= 7
+      :lose
+    else
+      :play
+    end
+  end
+
+  # Get a word from remote "random word" service
   def self.get_random_word
     require 'uri'
     require 'net/http'
